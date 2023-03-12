18c586f70c461354ac1fe75e3d3e99b7d793a41a
Haman <haman.shareghi@gmail.com>
2023-03-12 23:50:18 +0400

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace PerfectPay;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void TxtBill_OnCompleted(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Slider_OnValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Minus_OnClicked(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Plus_OnClicked(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Button_OnClicked(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
+
